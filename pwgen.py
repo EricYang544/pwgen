1526ab69071bdb5bcd6fe863a094ec81653a3ede
Eric Yang <ericyang544@gmail.com>
2023-11-26 18:44:24 +0900

    password generator

--- pwgen.py
+++ pwgen.py
@@ -12,57 +12,53 @@ using_symbols = input("Do you want to use symbols? (y/n): ")
 pw_length = int(input("(If you enter 0, the program will generate 12 digits password.)\nHow many digits do you want for your password?: "))
 
 if pw_length == 0:
-  if using_symbols == 'n':
-    password_lsit = []
-    for num in range(12):
-      password_lsit += random.choice(numbers)
+    if using_symbols == 'n':
+        password_lsit = []
+        for num in range(12):
+            password_lsit += random.choice(numbers)
   
-    password = "".join(password_lsit)
-    print(f"Your password is '{password}'")
+        password = "".join(password_lsit)
+        print(f"Your password is '{password}'")
   
-  elif using_symbols == 'y':
-    num_sym = numbers + symbols
-    password_list = []
-    for char in range(12):
-      password_list += random.choice(num_sym)
+    elif using_symbols == 'y':
+        num_sym = numbers + symbols
+        password_list = []
+        for char in range(12):
+            password_list += random.choice(num_sym)
   
-    password = "".join(password_list)
-    print(f"Your password is '{password}'")
+        password = "".join(password_list)
+        print(f"Your password is '{password}'")
 
-  else:
-    print("You have to enter 'y' or 'n'." )
+    else:
+        print("You have to enter 'y' or 'n'." )
 
-copy2clip(password)
-print("The password is copied to the clipboard.")
+    copy2clip(password)
+    print("The password is copied to the clipboard.")
 
-the_end = input("If you want to end this program, press enter.")
-if the_end == "":
-  quit()
+    the_end = input("If you want to end this program, press enter.")
 
-elif pw_length != 0:
-  if using_symbols == 'n':
-    password_lsit = []
-    for num in range(0,pw_length):
-      password_lsit += random.choice(numbers)
+if pw_length != 0:
+    if using_symbols == 'n':
+        password_lsit = []
+        for num in range(0,pw_length):
+            password_lsit += random.choice(numbers)
 
-    password = "".join(password_lsit)
-    print(f"Your password is '{password}'")
+        password = "".join(password_lsit)
+        print(f"Your password is '{password}'")
 
-  elif using_symbols == 'y':
-    num_sym = numbers + symbols
-    password_list = []
-    for char in range(0, pw_length):
-      password_list += random.choice(num_sym)
+    elif using_symbols == 'y':
+        num_sym = numbers + symbols
+        password_list = []
+        for char in range(0, pw_length):
+            password_list += random.choice(num_sym)
 
-    password = "".join(password_list)
-    print(f"Your password is '{password}'")
+        password = "".join(password_list)
+        print(f"Your password is '{password}'")
 
-  else:
-    print("You have to enter 'y' or 'n'." )
+    else:
+        print("You have to enter 'y' or 'n'." )
 
-copy2clip(password)
-print("The password is copied to the clipboard.")
+    copy2clip(password)
+    print("The password is copied to the clipboard.")
 
-the_end = input("If you want to end this program, press enter.")
-if the_end == "":
-  quit()
+    the_end = input("If you want to end this program, press enter.")
